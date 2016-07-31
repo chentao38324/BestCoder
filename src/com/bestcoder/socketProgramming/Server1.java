436514ea627ee7290d0158c06a8e64a558732470
Chen Tao <1457562356@qq.com>
2016-07-31 14:50:48 +0800

    socket编程 客户端 服务端

---
+++ src/com/bestcoder/socketProgramming/Server1.java
@@ -0,0 +1,29 @@
+package com.bestcoder.socketProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by chentao on 16-7-30.
+ * 建立服务器
+ */
+public class Server1 {
+    public static void main(String[] args) throws Exception{
+        //服务端在20006端口监听客户端请求的TCP连接
+        ServerSocket server = new ServerSocket(20006);
+        Socket client = null;
+        boolean f = true;
+        while(f){
+            //等待客户端的连接，如果没有获取连接
+            client = server.accept();
+            System.out.println("与客户端连接成功！");
+            //为每个客户端连接开启一个线程
+            new Thread(new ServerThread(client)).start();
+        }
+        server.close();
+    }
+}

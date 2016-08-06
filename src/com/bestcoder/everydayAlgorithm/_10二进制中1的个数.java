53bca8d65a48de1e60cc9f73f9c023b4ab3fd7de
Chen Tao <1457562356@qq.com>
2016-08-06 12:07:30 +0800

    Fibonacci数列和二进制中1的个数 两道算法

---
+++ src/com/bestcoder/everydayAlgorithm/_10二进制中1的个数.java
@@ -0,0 +1,54 @@
+package com.bestcoder.everydayAlgorithm;
+
+/**
+ * Created by chentao on 16-8-6.
+ * 三种方法
+ */
+public class _10二进制中1的个数 {
+    public static void main(String[] args){
+        int n = -15;
+        int method1 = numberOfOne1(n);
+        int method2 = numberOfOne2(n);
+        int method3 = numberOfOne3(n);
+        System.out.println("方法一："+method1+"\n方法二："+method2+"\n方法三："+method3);
+    }
+
+
+
+    //方法一：从整数右边开始判断右边第一位是不是1（用整数和1做与运算，=1是1，=0不是1）然后整数右移1位（n>>1）
+    public static int numberOfOne1(int n){
+        int count = 0;      //记录1的个数
+        while (n != 0){
+            if ((n&1)==1){
+                count++;
+            }
+            n = n >>> 1;  //n右移1位
+        }
+        return count;
+    }
+    //方法二：不移动n，移动1，先将n和1与运算，知道最右边一位是不是1（=1是1，=0不是1）
+    // 然后1左移一位，n和1与运算，知道右边倒数第二位是不是1
+    private static int numberOfOne2(int n) {
+        int count = 0;
+        int flag = 1;
+        while (flag != 0){
+            if ((n&flag) != 0){
+                count++;
+            }
+            flag = flag<<1;
+        }
+        return count;
+    }
+
+    //方法三：1和2都需要循环n的位数次 这种方法 n中多少各1 就循环多少次 好方法
+    //结论：把一个数-1再和原来的数相与，会把原来整数最右边一个1变成0，有多少个1 就会执行多少次
+    private static int numberOfOne3(int n) {
+        int count = 0;
+        while (n != 0){
+            count++;
+            n = n&(n-1);
+        }
+        return count;
+    }
+
+}
---
+++ src/com/bestcoder/everydayAlgorithm/_9Fibonacci数列.java
@@ -0,0 +1,62 @@
+package com.bestcoder.everydayAlgorithm;
+
+/**
+ * Created by chentao on 16-8-6.
+ * 求斐波那契数列 第n项
+ * 除了第1,2项为0,1外其余每项等于前两项之和
+ * 递归实现 代码简单效率很低
+ * 循环实现 时间复杂度O(n)
+ *
+ * 拓展：青蛙跳台阶，一次只能调1或2级台阶，问跳到第n级台阶有多少种跳法
+ *
+ */
+public class _9Fibonacci数列 {
+    public static void main(String[] args){
+        _9Fibonacci数列 instance = new _9Fibonacci数列();
+        long timeStart,timeEnd;
+        timeStart = System.currentTimeMillis();
+        long n = instance.fibonacciInterative(40);
+        timeEnd = System.currentTimeMillis();
+        System.out.println("Fibonacci第40项循环实现："+n+" 运行时间(ms)："+(timeEnd-timeStart));
+        timeStart = System.currentTimeMillis();
+        n = instance.fibonacciRecursive(40);//非常非常慢
+        timeEnd = System.currentTimeMillis();
+        System.out.println("Fibonacci第40项递归实现："+n+" 运行时间(ms)："+(timeEnd-timeStart));
+    }
+    //循环实现
+    public long fibonacciInterative(int n){
+        if (n<2 && n>=0){
+            return n;
+        }
+        long fn_1 = 1;
+        long fn_2 = 0;
+        long fn = 0;
+        for (int i = 0; i < n; i++) {
+            fn = fn_1 + fn_2;
+            fn_2 = fn_1;
+            fn_1 = fn;
+        }
+        return fn;
+    }
+    //递归 效率低 开发不会用
+    public long fibonacciRecursive(int n){
+        if (n<=0) return 0;
+        if (n==1) return 1;
+        return fibonacciRecursive(n-1)+fibonacciRecursive(n-2);
+    }
+    //青蛙跳n级台阶的跳法种数
+    public long frogJumpStais(int n){
+        if (n>=0 && n<=2){
+            return n;
+        }
+        long fn_1 = 2;
+        long fn_2 = 1;
+        long fn = 0;
+        for (int i = 3; i < n; i++) {
+            fn = fn_2 + fn_1;
+            fn_2 = fn_1;
+            fn_1 = fn;
+        }
+        return fn;
+    }
+}

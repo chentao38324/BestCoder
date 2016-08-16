6f4cdec4cecac931d4133d35a9ea7977b6f7d9ae
Chen Tao <1457562356@qq.com>
2016-08-16 15:54:56 +0800

    从尾到头打印链表的修改

--- src/com/bestcoder/everydayAlgorithm/_5从尾到头打印链表_链表.java
+++ src/com/bestcoder/everydayAlgorithm/_5从尾到头打印链表_链表.java
@@ -1,58 +1,65 @@
 package com.bestcoder.everydayAlgorithm;
 
-import com.bestcoder.dataStructure.Node2;
 
 import java.util.Stack;
 
 /**
  * Created by chentao on 16-8-2.
+ * 题目：输入个链表的头结点，从尾到头反过来打印出每个结点的值。
+ * 
  * 思路：从尾到头打印 从前向后遍历，放入栈 先入后出可实现
  * 递归实现，递归本质是一个栈，每访问到一个节点的时候，先递归输出它后面的节点，在输出该节点自身
  */
 public class _5从尾到头打印链表_链表 {
     public static void main(String[] args) {
-        Node2 node5 = new Node2(null);
-        node5.setElement(5);
-        Node2 node4 = new Node2(4,node5);
-        Node2 node3 = new Node2(3,node4);
-        Node2 node2 = new Node2(2,node3);
-        Node2 node1 = new Node2(1,node2);
-        Node2 head = new Node2(null);
-        head.setNext(node1);
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4,node5);
+        ListNode node3 = new ListNode(3,node4);
+        ListNode ListNode = new ListNode(2,node3);
+        ListNode node1 = new ListNode(1,ListNode);
         System.out.print("原始链表：");
-        Node2 tem = head.getNext();
-        while (tem.getNext() != null){
-            System.out.print(tem.toString()+" ");
-            tem = tem.getNext();
+        ListNode tem = node1;
+        while (tem != null){
+            System.out.print(tem.val+" ");
+            tem = tem.next;
         }
         System.out.println("\n从尾到头打印：");
         //栈实现
         System.out.print("栈实现：");
-        printReserveStack(head.getNext());
+        printReserveStack(node1);
         //递归实现
         System.out.print("\n递归实现：");
-        printReserveRecursion(head.getNext());
+        printReserveRecursion(node1);
     }
     //栈实现
-    private static void printReserveStack(Node2 head) {
+    private static void printReserveStack(ListNode head) {
         Stack<Integer> stack = new Stack<>();
-        Node2 tem = head;
+        ListNode tem = head;
         while (tem != null){
-            stack.push((int)tem.getElement());
-            tem = tem.getNext();
+            stack.push(tem.val);
+            tem = tem.next;
         }
         while (!stack.isEmpty()){
             System.out.print(stack.pop()+" ");
         }
     }
     //递归实现
-    private static void printReserveRecursion(Node2 head) {
-       if (head != null){
-           if (head.getNext() != null){
-               printReserveRecursion(head.getNext());
-           }
-       }
-        System.out.print(head.getElement()+" ");
+    private static void printReserveRecursion(ListNode head) {
+        if (head != null && head.next != null) {
+            printReserveRecursion(head.next);
+        }
+        System.out.print(head.val+" ");
     }
 
+    private static class ListNode{
+        int val;
+        ListNode next;
+        public ListNode(int val){
+            this.val = val;
+        }
+        public ListNode(int val,ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
